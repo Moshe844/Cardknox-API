17c6da40d245cb90a2e4a13eb92a81c746a518de
Moshe <arye6700@gmail.com>
2023-01-15 21:02:52 -0500

    update

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const request = require("request");
+
+const app = express();
+
+app.get("/reportjson", (req, res) => {
+    const xVersion = "5.0.0";
+    const xSoftwareName = "Cardknox";
+    const xSoftwareVersion = "2.1";
+    const { xKey } = req.query;
+    const { xCommand } = req.query;
+    const { xBeginDate } = req.query;
+    const { xEndDate } = req.query;
+    const {xToken}  = req.query;
+    const {xMaskedCardnumber} = req.query
+
+
+    const body = JSON.stringify({
+        xKey,
+        xCommand,
+        xVersion,
+        xSoftwareName,
+        xSoftwareVersion,
+        xBeginDate,
+        xEndDate,
+        xToken,
+        xMaskedCardnumber
+      });
+
+      request(
+        {
+          url: "https://x1.cardknox.com/reportjson",
+          method: "POST",
+          body: body,
+        },
+
+        (error, response, body) => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log(body);
+
+            
+  
+)})
